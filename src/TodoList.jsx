0729b73328787cff429f85d6284af585e58371c5
Fauzi Mulyana <fauzi.mulyanaa9@gmail.com>
2023-12-26 21:18:37 +0700

    fix project

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import deleteIcon from "./assets/delete.png";
+import EditIcon from "./assets/edit.png";
 
 export default function TodoList() {
   const [inputTask, setInputTask] = useState("");
   const [list, setList] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  const [editInputTask, setEditInputTask] = useState("");
 
   const handleInput = (e) => {
     setInputTask(e.target.value);
@@ -13,6 +16,7 @@ export default function TodoList() {
     const newTask = {
       id: Math.random(),
       todo: todo,
+      completed: false,
     };
 
     setList([...list, newTask]);
@@ -35,27 +39,55 @@ export default function TodoList() {
     localStorage.setItem("list", JSON.stringify(list));
   }, [list]);
 
+  const handleEdit = (todo) => {
+    setEditTodo(todo.id);
+    setEditInputTask(todo.todo);
+  };
+  const handleSaveEdit = (id) => {
+    const updatedList = list.map((todo) => (todo.id === id ? { ...todo, todo: editInputTask } : todo));
+    setList(updatedList);
+    setEditTodo(null);
+  };
+  const handleComplete = (id) => {
+    const updatedList = list.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+    setList(updatedList);
+  };
+
   return (
     <>
-      <div className="container-fluid overflow-hidden h-screen flex justify-center items-star py-5 bg-[url('/bg.jpg')]">
-        <div className="wrapper-todo shadow-2xl p-5 h-auto rounded-md bg-white ">
+      <div className="container-fluid overflow-hidden h-screen flex justify-center items-star py-5 bg-gradient-to-r from-gray-700 via-gray-900 to-black">
+        <div className="wrapper-todo  p-5 h-auto rounded-md  border-2 border-white">
           <div className="tagline text-center">
-            <h1 className="text-3xl font-bold text-black">Todo List</h1>
+            <h1 className="text-4xl font-bold text-white ">Todo List</h1>
           </div>
           <div className="input-wrapper text-center mt-5">
-            <input className="border-2 outline-none border-slate-300 w-[350px] p-2 mr-7 rounded-md" type="text" value={inputTask} placeholder="Enter Task" onChange={handleInput} />
+            <input className="border-2 outline-none  w-[350px] p-2 mr-7 rounded-md" type="text" value={inputTask} placeholder="Enter Task" onChange={handleInput} />
             <button className="bg-teal-500 w-[100px] p-2 text-white rounded-xl" onClick={() => handleTodo(inputTask)}>
               ADD
             </button>
           </div>
-          <div className="todo-wrapper m-auto mt-6 w-[600px] h-[300px] rounded-lg">
+          <div className="todo-wrapper m-auto mt-6 w-[600px] h-[400px] rounded-lg overflow-y-auto ">
             <ul className="w-full p-5">
               {list.map((todo) => (
-                <li key={todo.id} className="flex justify-between mb-5 bg-yellow-200 px-4 py-4">
-                  <p className="text-lg">{todo.todo}</p>
-                  <button onClick={() => handleDelete(todo.id)}>
-                    <img src={deleteIcon} alt="delete icon" />
-                  </button>
+                <li key={todo.id} className={`flex justify-between mb-5 px-4 py-4 ${todo.completed ? "line-through text-gray-500" : "bg-[#FFF7D4]"}`}>
+                  {editTodo === todo.id ? <input type="text" value={editInputTask} onChange={(e) => setEditInputTask(e.target.value)} className="p-2" /> : <p className="text-lg">{todo.todo}</p>}
+                  <div className="flex items-center gap-3">
+                    {editTodo === todo.id ? (
+                      <button onClick={() => handleSaveEdit(todo.id)}>Save</button>
+                    ) : (
+                      <>
+                        <button onClick={() => handleComplete(todo.id)} className="text-red-900">
+                          {todo.completed ? "Undo" : "Complete"}
+                        </button>
+                        <button onClick={() => handleEdit(todo)}>
+                          <img src={EditIcon} alt="edit icon" className="w-[19px] h-[19px]" />
+                        </button>
+                      </>
+                    )}
+                    <button onClick={() => handleDelete(todo.id)}>
+                      <img src={deleteIcon} alt="delete icon" />
+                    </button>
+                  </div>
                 </li>
               ))}
             </ul>
